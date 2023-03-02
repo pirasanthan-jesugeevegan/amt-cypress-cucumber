6ea200dfc60a04193575b1a42ec9fb2ca568fc62
Pirasanthan Jesugeevegan <pirasanthanjesugeevegan@Pirasanthans-MacBook-Pro.local>
2023-03-02 16:28:08 +0000

    logging images

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -12,6 +12,8 @@ docker build \
 
 docker push -q ${UI_RUNNER_DOCKER_IMAGE_REV}
 
+echo $UI_RUNNER_DOCKER_IMAGE_REV
+echo $UI_RUNNER_DOCKER_IMAGE_CACHE
 if [ "##[set-output name=branch;]${GITHUB_REF#refs/heads/}" == "master" ]
 then
     docker tag ${UI_RUNNER_DOCKER_IMAGE_REV} ${UI_RUNNER_DOCKER_IMAGE_CACHE}
--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DOCKER_BUILDKIT=1
 ECR_PATH=325398057325.dkr.ecr.us-east-1.amazonaws.com
 UI_RUNNER_DOCKER_IMAGE=${ECR_PATH}/web-ui-amt
 UI_RUNNER_DOCKER_IMAGE_CACHE=${UI_RUNNER_DOCKER_IMAGE}:latest
